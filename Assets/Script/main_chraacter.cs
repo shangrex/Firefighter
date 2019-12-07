a11e3dfb9def8d8f5237a47f1d201d1d8e048f32
huangrex <rexhuang881019@gmail.com>
2019-12-07 23:41:01 +0800

    add wall human ground

---
+++ Assets/Script/main_chraacter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main_chraacter : MonoBehaviour {
+
+    private int mode = 3;
+    // Use this for initialization
+    void Start () {
+        transform.localPosition = new Vector3(2.17f, 1.878f, 3.363f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (mode == 1)
+            {
+                mode = 3;
+            }
+            else if (mode == 3)
+            {
+                mode = 1;
+            }
+        }
+        if (mode == 3)
+        {
+            transform.localPosition = new Vector3(-0.06f, 1.70f, -3.17f);
+            Cursor.lockState = CursorLockMode.None;
+        }
+        if (mode == 1)
+        {
+            transform.localPosition = new Vector3(-0.06f, 1.17f, 0.56f);
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
